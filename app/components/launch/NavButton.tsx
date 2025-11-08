1bd7c775d100133cfa67dfa1fba6481edc12665a
Celeste Roselli <72556836+celesteroselli@users.noreply.github.com>
2025-11-07 22:53:09 -0500

    changed font from ttf to woff

--- app/components/launch/NavButton.tsx
+++ app/components/launch/NavButton.tsx
@@ -29,18 +29,12 @@ export default function NavButton({ name, href, onClick }: NavButtonProps) {
         onClick={onClick}
         className="flex w-full items-center justify-center text-center py-3 px-3"
       >
-        <span className="font-a nav-color">
+        <span className="font-[Augustus] nav-color">
           <h2 className="nav-text m-0 leading-none">{name}</h2>
         </span>
       </Link>
     </div>
   );
-    return <div className={newClasses}>
-    <Link
-    href={href}
-    onClick={onClick}
-    ><span className='text-center font-[Augustus] nav-color'><h2 className='nav-text'>{name}</h2></span></Link>
-    </div>;
 }
 
 
